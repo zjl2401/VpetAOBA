fff09734a8c71539de4e97beb9cab091c9cb342e
zjl2401 <3625547823@qq.com>
2026-07-14 22:18:47 +0800

    Summary

--- VpetPNG/1.2/release/启动桌宠.bat
+++ VpetPNG/1.2/release/启动桌宠.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001 >nul
-start "" "C:\Users\36255\Desktop\VpetAOBA\VpetPNG\1.2\release\Vpet_new\Vpet.exe"
+start "" "C:\Users\36255\Desktop\VpetAOBA\VpetPNG\1.2\release\Vpet\Vpet.exe"
